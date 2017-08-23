21522a13ff7dec4ea4b025e11bde9eee671d1acf
martin <telemagico@gmail.com>
2017-08-23 13:51:59 -0300

    sections select data

--- www/js/autorizaciones.js
+++ www/js/autorizaciones.js
@@ -1,38 +1,34 @@
-  var currentnode = '/cuentas/' + id
-  , cuentas = firebase.database().ref(currentnode)
-  , datosdeapoyo = {}
+  var currentnode = '/autorizaciones/'+id
+  , reservas = firebase.database().ref(currentnode)
+  , datosdeapoyo = {}  
   , anim = helper.animation
 
-  cuentas.once('value').then(function(datos) {
+  reservas.once('value').then(function(datos) {
     if(!datos.val()){
       $('.spinner').fadeOut(anim.transition.fadeOut, function(){
         $('.lista').delay(anim.transition.delay).fadeIn()
       })    
     }
   })
-    
+
   firebase.database().ref('/datosdeapoyo').once('value').then(function(datos) {
     datosdeapoyo = datos.val()
   })
 
   $(document).on('submit','#firebase-form',function(e){
     e.preventDefault()
+    
     var data = $(this).serializeObject()
     , updates = {}
-    , key = $(this).attr('key')
+    , key = data.tag 
 
-    delete data.contacto_email
-    delete data.contacto_telefono
-    delete data.contacto_empresa
-    delete data.contacto_cuentas
-    delete data.contacto_empleados
-
-    if(key){
-      updates[currentnode + '/' + key] = data
-    } else {
-      var newKey = cuentas.push().key
-      updates[currentnode + '/' + newKey] = data
+    if(!data.tag){
+      return swal("Se necesitan mas datos", "Ingresa etiqueta","warning")
     }
+
+    delete data.tag 
+      
+    updates[currentnode +'/' + key] = data
 
     $('.spinner').fadeIn(anim.transition.fadeIn, function(){
       firebase.database().ref().update(updates, function(error){
@@ -40,9 +36,9 @@
           console.log(error)
         }else{
           $('#detail').fadeOut(anim.transition.fadeOut,function(){
-            $('.lista').delay(anim.transition.delay).fadeIn(anim.transition.fadeIn,function(){
-              helper.resetScroll()
-              $('.spinner').fadeOut(anim.transition.fadeOut)
+            $('.lista').fadeIn(anim.transition.fadeIn,function(){
+              //helper.resetScroll()
+              $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor)
             })
           }) 
         }
@@ -52,8 +48,8 @@
     return false  
   })
 
-  $(document).on('click','.add-cuenta',function(e){
-    $('#detail').html($.templates('#form').render({key:null,data:{plan:""},datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
+  $(document).on('click','.add-item',function(e){
+    $('#detail').html($.templates('#form').render({key:null,data:{estado:""},aux:helper.aux.reservas,datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
       $('.lista').fadeOut(anim.transition.fadeOut,function(){
         $('#detail').delay(200).fadeIn(anim.transition.fadeOut*anim.transition.factor,function(){
           $('body,html').scrollTop(0)
@@ -66,11 +62,11 @@
     var key = $(this).data('key')
     $('body').attr('key',key)
     helper.setScroll()
-    $('.spinner').fadeIn(anim.transition.fadeIn*anim.transition.factor, function(){
-      firebase.database().ref(currentnode+'/'+key).once('value').then(function(cuenta) {
-        $('#detail').html($.templates('#form').render({key:cuenta.key,data:cuenta.val(),datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
+    $('.spinner').fadeIn(anim.transition.fadeIn*anim.transition.factor, function(){  
+      firebase.database().ref(currentnode +'/'+key).once('value').then(function(item) {
+        $('#detail').html($.templates('#form').render({key:item.key,data:item.val(),aux:helper.aux.reservas,datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
           $('.lista').fadeOut(anim.transition.fadeOut,function(){
-            $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor,function(){
+            $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor,function(){                    
               $('#detail').delay(200).fadeIn(anim.transition.fadeOut*anim.transition.factor,function(){
                 $('body,html').scrollTop(0)
               })
@@ -84,14 +80,14 @@
   $(document).on('click','.action.eliminar',function(){
     var key = $(this).data('key')
     swal({   
-      title: "Borrar cuenta",   
-      text: "Seguro que querés eliminar esta cuenta?",   
+      title: "Borrar reserva",   
+      text: "Seguro que querés eliminar esta reserva?",
       type: "warning",
       showCancelButton: true,   
       closeOnConfirm: false,   
       showLoaderOnConfirm: true,
     }, function(){    
-      firebase.database().ref(currentnode + '/' + key).remove().then(function(){
+      firebase.database().ref(currentnode + key).remove().then(function(){
         swal.close()
       })
     })
@@ -106,24 +102,24 @@
   })  
 
   // live fb handlers
-  cuentas.on('child_added', (data) => {
-    $('#list').prepend($.templates('#item').render({key:data.key,data:data.val()}, helper)).promise().done(function(){
+  reservas.on('child_added', (data) => {
+    $('#list').prepend($.templates('#item').render({key:data.key,data:data.val()}, helper.aux.reservas)).promise().done(function(){
       $('#list').find('#'+data.key).animateAdded()
     })  
     $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor, function(){
       $('.lista').delay(anim.transition.delay).fadeIn()
-    })  
+    })
   })
 
-  cuentas.on('child_changed', (data) => {
+  reservas.on('child_changed', (data) => {
     var index = $('#'+data.key).index()
     $('#'+data.key).remove()
     $('#list').insertAt(index, $.templates('#item').render({key:data.key,data:data.val()}))
     $('#'+data.key).animateChanged()
   })
 
-  cuentas.on('child_removed', (data) => {
+  reservas.on('child_removed', (data) => {
     $('#'+data.key).animateRemoved(function(){
       $(this).remove()  
-    })  
+    })    
   })
--- www/js/publicaciones.js
+++ www/js/publicaciones.js
@@ -1,38 +1,35 @@
-  var currentnode = '/cuentas/' + id
-  , cuentas = firebase.database().ref(currentnode)
-  , datosdeapoyo = {}
+  var currentnode = '/publicaciones/'+id
+  , publicaciones = firebase.database().ref(currentnode)
+  , datosdeapoyo = {}  
   , anim = helper.animation
 
-  cuentas.once('value').then(function(datos) {
+  publicaciones.once('value').then(function(datos) {
     if(!datos.val()){
       $('.spinner').fadeOut(anim.transition.fadeOut, function(){
         $('.lista').delay(anim.transition.delay).fadeIn()
       })    
     }
   })
-    
+
   firebase.database().ref('/datosdeapoyo').once('value').then(function(datos) {
     datosdeapoyo = datos.val()
   })
 
+
   $(document).on('submit','#firebase-form',function(e){
     e.preventDefault()
+    
     var data = $(this).serializeObject()
     , updates = {}
-    , key = $(this).attr('key')
+    , key = data.tag 
 
-    delete data.contacto_email
-    delete data.contacto_telefono
-    delete data.contacto_empresa
-    delete data.contacto_cuentas
-    delete data.contacto_empleados
-
-    if(key){
-      updates[currentnode + '/' + key] = data
-    } else {
-      var newKey = cuentas.push().key
-      updates[currentnode + '/' + newKey] = data
+    if(!data.tag){
+      return swal("Se necesitan mas datos", "Ingresa etiqueta","warning")
     }
+
+    delete data.tag 
+      
+    updates[currentnode +'/' + key] = data
 
     $('.spinner').fadeIn(anim.transition.fadeIn, function(){
       firebase.database().ref().update(updates, function(error){
@@ -40,9 +37,9 @@
           console.log(error)
         }else{
           $('#detail').fadeOut(anim.transition.fadeOut,function(){
-            $('.lista').delay(anim.transition.delay).fadeIn(anim.transition.fadeIn,function(){
-              helper.resetScroll()
-              $('.spinner').fadeOut(anim.transition.fadeOut)
+            $('.lista').fadeIn(anim.transition.fadeIn,function(){
+              //helper.resetScroll()
+              $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor)
             })
           }) 
         }
@@ -52,8 +49,8 @@
     return false  
   })
 
-  $(document).on('click','.add-cuenta',function(e){
-    $('#detail').html($.templates('#form').render({key:null,data:{plan:""},datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
+  $(document).on('click','.add-publicacion',function(e){
+    $('#detail').html($.templates('#form').render({key:null,data:{estado:""},aux:helper.aux.publicaciones,datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
       $('.lista').fadeOut(anim.transition.fadeOut,function(){
         $('#detail').delay(200).fadeIn(anim.transition.fadeOut*anim.transition.factor,function(){
           $('body,html').scrollTop(0)
@@ -66,11 +63,12 @@
     var key = $(this).data('key')
     $('body').attr('key',key)
     helper.setScroll()
-    $('.spinner').fadeIn(anim.transition.fadeIn*anim.transition.factor, function(){
-      firebase.database().ref(currentnode+'/'+key).once('value').then(function(cuenta) {
-        $('#detail').html($.templates('#form').render({key:cuenta.key,data:cuenta.val(),datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
+    $('.spinner').fadeIn(anim.transition.fadeIn*anim.transition.factor, function(){  
+      firebase.database().ref(currentnode +'/'+key).once('value').then(function(publicacion) {
+        console.log(publicacion.val())
+        $('#detail').html($.templates('#form').render({key:publicacion.key,data:publicacion.val(),aux:helper.aux.publicaciones,datosdeapoyo:datosdeapoyo},helper)).promise().done(function(){
           $('.lista').fadeOut(anim.transition.fadeOut,function(){
-            $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor,function(){
+            $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor,function(){                    
               $('#detail').delay(200).fadeIn(anim.transition.fadeOut*anim.transition.factor,function(){
                 $('body,html').scrollTop(0)
               })
@@ -84,14 +82,14 @@
   $(document).on('click','.action.eliminar',function(){
     var key = $(this).data('key')
     swal({   
-      title: "Borrar cuenta",   
-      text: "Seguro que querés eliminar esta cuenta?",   
+      title: "Borrar página",   
+      text: "Seguro que querés eliminar esta página?",
       type: "warning",
       showCancelButton: true,   
       closeOnConfirm: false,   
       showLoaderOnConfirm: true,
     }, function(){    
-      firebase.database().ref(currentnode + '/' + key).remove().then(function(){
+      firebase.database().ref(currentnode + key).remove().then(function(){
         swal.close()
       })
     })
@@ -106,24 +104,24 @@
   })  
 
   // live fb handlers
-  cuentas.on('child_added', (data) => {
-    $('#list').prepend($.templates('#item').render({key:data.key,data:data.val()}, helper)).promise().done(function(){
+  publicaciones.on('child_added', (data) => {
+    $('#list').prepend($.templates('#item').render({key:data.key,data:data.val()}, helper.aux.publicaciones)).promise().done(function(){
       $('#list').find('#'+data.key).animateAdded()
     })  
     $('.spinner').fadeOut(anim.transition.fadeOut*anim.transition.factor, function(){
       $('.lista').delay(anim.transition.delay).fadeIn()
-    })  
+    })
   })
 
-  cuentas.on('child_changed', (data) => {
+  publicaciones.on('child_changed', (data) => {
     var index = $('#'+data.key).index()
     $('#'+data.key).remove()
     $('#list').insertAt(index, $.templates('#item').render({key:data.key,data:data.val()}))
     $('#'+data.key).animateChanged()
   })
 
-  cuentas.on('child_removed', (data) => {
+  publicaciones.on('child_removed', (data) => {
     $('#'+data.key).animateRemoved(function(){
       $(this).remove()  
-    })  
+    })    
   })
